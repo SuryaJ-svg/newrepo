e9b2d133bb5ca7c44e70ebf84adcc960e3201ec4
SuryaJ-svg <surya.jeeda@gmail.com>
2024-08-08 19:22:45 -0400

    commit

---
+++ Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    public static void main(String[] args) {
+        int x = 16;
+        age.currentage(x);
+        age.older(x);
+        age.younger(x);   
+    }
+}
+class age {
+    public Static void currentage int (x) {
+        System.out.println("x");
+        
+    }
+
+    public Static void older int (x) {
+        x++;
+        System.out.println("x");
+    }
+
+    public Static void younger int (x) {
+        x--;
+        System.out.println("x");
+    }
+    
+}
