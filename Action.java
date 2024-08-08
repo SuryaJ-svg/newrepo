14f3ed5fcc62caf083c68933230be28feda772a0
SuryaJ-svg <surya.jeeda@gmail.com>
2024-08-08 19:34:27 -0400

    commit

--- Action.java
+++ Action.java
@@ -1,3 +1,8 @@
 public class Action {
-    
+    public static void main(String[] args) {
+        Person obj = new Person();
+        obj.currentage();
+        obj.older();
+        obj.younger();
+}
 }
--- Person.java
+++ Person.java
@@ -1,25 +1,21 @@
 public class Person {
     public static void main(String[] args) {
-        int x = 16;
-        age.currentage(x);
-        age.older(x);
-        age.younger(x);   
-    }
-}
-class age {
-    public Static void currentage int (x) {
-        System.out.println("x");
+        int age = 16;
+
+    public Static void currentage int (age) {
+        System.out.println(age);
         
     }
 
-    public Static void older int (x) {
-        x++;
-        System.out.println("x");
+    public Static void older int (age) {
+        age++;
+        System.out.println(age);
     }
 
-    public Static void younger int (x) {
-        x--;
-        System.out.println("x");
+    public Static void younger int (age) {
+        age--;
+        System.out.println(age);
     }
     
+}
 }
