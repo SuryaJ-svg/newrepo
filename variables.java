56b1c156359558a1697a204f0bb2f7cc9ef17040
SuryaJ-svg <surya.jeeda@gmail.com>
2024-08-08 16:40:11 -0400

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) throws Exception {
+
+        int number = 5;
+        System.out.println(number);
+
+        final String WORD = "hamerheads";
+        System.out.println(WORD);
+
+    }
+}
