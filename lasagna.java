a3fb51bd89d0424f21bf11796ad4801a6e4d1269
SuryaJ-svg <surya.jeeda@gmail.com>
2024-08-09 19:21:03 -0400

    commit

---
+++ lasagna.java
@@ -0,0 +1,13 @@
+public class lasagna {
+    public static void main (String args[]) {
+        int minutes;
+        expectedMinutesInOven obj = new expectedMinutesInOven();
+        System.out.println(obj.expectedMinutesInOven());
+        System.out.println(minutesNew);
+        
+    }
+    void expectedMinutesInOven(){
+        int minutesNew = 40;
+    }
+
+}
