5868e9443f7ea7ec7e92e9506cdb09b8714c0268
SuryaJ-svg <surya.jeeda@gmail.com>
2024-08-11 19:05:31 -0400

    Update lasagna.java

--- lasagna.java
+++ lasagna.java
@@ -1,33 +1,24 @@
-/* part 2
 public class lasagna {
-    int cookingTime = 40;
 
-    public int expectedMinutesInOven(){
-        return cookingTime;
-    }
-
-    public int remainingMinutesInOven(int minPassed){
-        return cookingTime - minPassed;
-    }
-
+    int ExpectedCookingTime = 50;
+    
     public int preparationTime(int layers){
         return layers * 2;
     }
-
-}
-*/
-//part 3
-class totalTimeInMinutes(){
-    public int totalTime (int cookingTime, int layers) {
-        return cookingTime+layers;
+    public int expectedMinutesInOven(int minutesInOven){
+        return ExpectedCookingTime - minutesInOven;
     }
+    public int totalTimeSpent (int minutesInOven, int preparationTime) {
+        return minutesInOven + preparationTime(5);
+        //call a method within a mehtod under the same scope wihtout creating a new object
+    }
+
+    public static void main (String args[]) {
+
+    lasagna obj = new lasagna();
+
+    System.out.println(obj.preparationTime(5));
+    System.out.println(obj.expectedMinutesInOven(20));
+    System.out.println(obj.totalTimeSpent(20,5));
 }
-
-public static void main (String args[]) {
-    int cookingTime = 40;
-    int layers = 5;
-    
-   totalTimeInMinutes obj = new totalTimeInMinutes();
-   System.out.println(obj.totalTimeInMinutes(cookingTime, layers));
-
 }
